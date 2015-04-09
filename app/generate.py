667204e7157301e6c7e37d330582ad67f0714a66
Tathagata <tathagatadg@gmail.com>
2015-04-09 17:49:15 -0500

    Added reading from xlsx

--- app/generate.py
+++ app/generate.py
@@ -3,23 +3,27 @@ import csv
 import jinja2
 from app.defaults import templateVars
 import os
+import openpyxl
+import logging
 
+logger = logging.getLogger()
 
 templateLoader = jinja2.FileSystemLoader(os.path.dirname(os.path.dirname(__file__))+"/templates")
 templateEnv = jinja2.Environment(loader=templateLoader)
 static_output_loc = 'static'
 
 TEMPLATE_FILES = ["index.html", "profile.html"]
-# TEMPLATE_FILES = ["profile.html"]
 templates = map(templateEnv.get_template, TEMPLATE_FILES)
 
 def render_static_files(**profile_data):
-    # print profile_data
     for template in templates:
-        print profile_data['Why are you running for Asha for Education?']
+        logger.debug("blurb {0}".format(profile_data['Why are you running for Asha for Education?']))
         output_text = template.render(FirstName=profile_data['First Name'],
                                      LastName=profile_data['Last Name'],
-                                     MarathonReason=profile_data['Why are you running for Asha for Education?'])
+                                     MarathonReason=profile_data['Why are you running for Asha for Education?'],
+                                     channelid=int(profile_data['Channelid']))
+                                     # donorcount=int(profile_data['DonorCount']),
+                                     # totalAmount=int(profile_data['TotalAmount']))
 
         dir_name = profile_data['First Name'] + profile_data['Last Name']
 
@@ -51,32 +55,26 @@ def load_csv(filename):
 
 def load_xlsx(filename):
     def sanitize(value):
-        print value, type(value)
         if not isinstance(value, unicode):
             return value
 
-        v = value.replace("\u201c", "'").replace("\u2013", "'").replace("\u201d", "").replace("\u2015", "")
-        return v
+        return value.replace("\u201c", "'").replace("\u2013", "'").replace("\u201d", "").replace("\u2015", "")
 
-    import openpyxl
-
-    workbook = openpyxl.load_workbook(filename='data/data.xlsx', use_iterators=True)
+    workbook = openpyxl.load_workbook(filename=filename, use_iterators=True)
     worksheet = workbook.get_sheet_by_name('Form Responses 1')
     rows = worksheet.rows
+
     header = [cell.value for cell in rows.next()]
     data_list = []
+
     for row in rows:
-        # print [sanitize(cell.value) for cell in row]
         data_list.append((dict(zip(header, [sanitize(cell.value) for cell in row]))))
-    # pp( data_list)
     return filter(lambda x: x['First Name'] is not None and x.get('Channelid') is not None, data_list)
 
 
 
 def csv_to_jinja(filename):
-    # all_profile_data=load_csv(filename)
     all_profile_data = load_xlsx(filename)
-    print all_profile_data
     if all_profile_data is not None:
         runners_data_list = [('%s %s' % (profile_data['First Name'], profile_data['Last Name']),
                           'users/%s%s/profile.html' % (profile_data['First Name'], profile_data['Last Name']))
@@ -88,9 +86,7 @@ def csv_to_jinja(filename):
         for profile_data in all_profile_data:
             render_static_files(**profile_data)
 
-
 if __name__ == '__main__':
-    filename = "data/data.xlsx"
+    filename = "data/data_old.xlsx"
 
     csv_to_jinja(filename)
-
