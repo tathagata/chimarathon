2cb6e59edcc02b5a6551e7ab99144784238322af
Tathagata <tathagatadg@gmail.com>
2015-04-18 14:08:07 -0500

    fixes for ui and content

--- app/generate.py
+++ app/generate.py
@@ -7,7 +7,8 @@ import os
 import openpyxl
 
 
-logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
+
 
 templateLoader = jinja2.FileSystemLoader(os.path.dirname(os.path.dirname(__file__))+"/templates")
 templateEnv = jinja2.Environment(loader=templateLoader)
@@ -16,30 +17,32 @@ static_output_loc = 'static'
 TEMPLATE_FILES = ["index.html", "profile.html"]
 templates = map(templateEnv.get_template, TEMPLATE_FILES)
 
-def render_static_files(**profile_data):
+def render_profile_pages(**profile_data):
     for template in templates:
-        logger.debug("blurb {0}".format(profile_data['Why are you running for Asha for Education?']))
+        print repr(profile_data['Why are you running for Asha for Education?'])
+
+
         output_text = template.render(FirstName=profile_data['First Name'],
                                      LastName=profile_data['Last Name'],
                                      MarathonReason=profile_data['Why are you running for Asha for Education?'],
-                                     channelid=int(profile_data['Channelid']))
-                                     # donorcount=int(profile_data['DonorCount']),
-                                     # totalAmount=int(profile_data['TotalAmount']))
+                                     channelid=int(profile_data['Channelid']),
+                                     donorcount=int(profile_data['DonorsCount']),
+                                     totalAmount=int(profile_data['TotalAmount']))
 
         dir_name = profile_data['First Name'] + profile_data['Last Name']
 
-        write_static_file("static/users/%s/profile.html" % dir_name, output_text)
+        write_templates_to_file("static/users/%s/profile.html" % dir_name, output_text)
 
 
-def render_index_file(runners_data_list):
+def render_index_page(runners_data_list):
     index = templateEnv.get_template("index.html")
 
     outputText = index.render(runners_data_list=runners_data_list,
                               **templateVars)
-    write_static_file("static/index.html", outputText)
+    write_templates_to_file("static/index.html", outputText)
 
 
-def write_static_file(filename, outputText):
+def write_templates_to_file(filename, outputText):
     if "profile" in filename:
         directory = filename[:-12]
         if not os.path.exists(directory):
@@ -50,12 +53,14 @@ def write_static_file(filename, outputText):
 
 def load_csv(filename):
     csv_file = open(filename, 'rb')
-    return filter(lambda x: x['First Name'] is not None and x.get('Channelid') is not None and len(x['Channelid']) > 0,
+    return filter(lambda x: x['First Name'] is not None, #and x.get('Channelid') is not None and len(x['Channelid']) > 0,
                   [row for row in csv.DictReader(csv_file, delimiter=",", quotechar='|') if row is not None])
 
 
 def load_xlsx(filename):
     def sanitize(value):
+        if value is None:
+            return 0
         if not isinstance(value, unicode):
             return value
 
@@ -70,24 +75,27 @@ def load_xlsx(filename):
 
     for row in rows:
         data_list.append((dict(zip(header, [sanitize(cell.value) for cell in row]))))
-    return filter(lambda x: x['First Name'] is not None and x.get('Channelid') is not None, data_list)
-
+    # return filter(lambda x: x['First Name'] is not None and x.get('Channelid') is not None, data_list)
+    return filter(lambda x: x['First Name'] is not None and x['First Name'] != 0, data_list)
 
 
 def csv_to_jinja(filename):
     all_profile_data = load_xlsx(filename)
     if all_profile_data is not None:
-        runners_data_list = [('%s %s' % (profile_data['First Name'], profile_data['Last Name']),
-                          'users/%s%s/profile.html' % (profile_data['First Name'], profile_data['Last Name']))
+        runners_data_list = [(profile_data['First Name'],
+                              profile_data['Last Name'],
+                              int(profile_data['DonorsCount']),
+                              int(profile_data['TotalAmount']),
+                             )
 
                          for profile_data in all_profile_data]
 
-        render_index_file(runners_data_list)
+        render_index_page(runners_data_list)
 
         for profile_data in all_profile_data:
-            render_static_files(**profile_data)
+            render_profile_pages(**profile_data)
 
 if __name__ == '__main__':
-    filename = "data/data_old.xlsx"
+    filename = "data/data20150418.xlsx"
 
     csv_to_jinja(filename)
